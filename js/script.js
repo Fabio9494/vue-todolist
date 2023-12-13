8d998d4aff6a708c5c513dfc165ed1b09796d7f5
Fabio9494 <fabio.casoria94@gmail.com>
2023-12-13 17:49:27 +0100

    add milestone 2

--- js/script.js
+++ js/script.js
@@ -3,31 +3,42 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            
             todoList:[
                 {
                    text: "cucinare",
-                   done: false
+                   done: false,
                 },
                 {
                     text: "bere",
-                    done: false
+                    done: false,
                  },
                  {
                     text: "mangiare",
-                    done: true
+                    done: false,
                  },
                  {
                     text: "salutare",
-                    done: false
+                    done: false,
                  },
                  {
                     text: "portare a spasso il cammello" ,
-                    done: true
+                    done: false,
                  },
             ]
         }
     },
     methods: {
-        
+        check(i){
+            if(!this.todoList[i].done){
+                this.todoList[i].done=true;
+            }
+        },
+        removeItem(i){
+            let conf = confirm("vuoi eliminare questo elemento?");
+            if(conf){
+                this.todoList.splice(i,1);
+            }
+        }
     },
 }).mount('#app');
